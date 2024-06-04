eb6849d2792911b31776b3de541b55711195e2ff
rabendra <rabendrasharma007@gmail.com>
2024-06-04 23:02:00 +0530

    code refactorization

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,21 +3,40 @@ class StringCalculator
   def add(string)
     return 0 if string.empty?
 
-    delimiter = /,|\n/
+    delimiter, numbers_string = extract_delimiter_and_numbers(string)
+    numbers = extract_numbers(numbers_string, delimiter)
+
+    validate_numbers(numbers)
+
+    numbers.reject { |n| n > 1000 }.sum
+  end
+
+  private
+
+  def extract_delimiter_and_numbers(string)
     if string.start_with?("//")
       parts = string.split("\n", 2)
-      if parts[0] =~ /\/\/(\[.+?\])+/
-        delimiter = parts[0].scan(/\[(.+?)\]/).map { |d| Regexp.escape(d[0]) }.join('|')
-      else
-        delimiter = Regexp.escape(parts[0][2])
-      end
-      string = parts[1]
+      delimiter = extract_custom_delimiters(parts[0])
+      [delimiter, parts[1]]
+    else
+      [/[,|\n]/, string]
     end
+  end
 
-    numbers = string.split(/#{delimiter}/).map(&:to_i).reject { |n| n > 1000 }
+  def extract_custom_delimiters(header)
+    if header =~ /\/\/(\[.+?\])+/
+      header.scan(/\[(.+?)\]/).map { |d| Regexp.escape(d[0]) }.join('|')
+    else
+      Regexp.escape(header[2])
+    end
+  end
+
+  def extract_numbers(numbers_string, delimiter)
+    numbers_string.split(/#{delimiter}/).map(&:to_i)
+  end
+
+  def validate_numbers(numbers)
     negatives = numbers.select { |n| n < 0 }
     raise "negatives not allowed: #{negatives.join(', ')}" unless negatives.empty?
-
-    numbers.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,65 +3,55 @@ require 'rspec'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
   describe '#add' do
     it 'returns 0 for an empty string' do
-      calculator = StringCalculator.new
       expect(calculator.add("")).to eq(0)
     end
 
     it 'returns the number itself for a single number' do
-      calculator = StringCalculator.new
       expect(calculator.add("1")).to eq(1)
     end
 
     it 'returns the sum of two numbers' do
-      calculator = StringCalculator.new
       expect(calculator.add("1,2")).to eq(3)
     end
 
     it 'returns the sum of an unknown amount of numbers' do
-      calculator = StringCalculator.new
       expect(calculator.add("1,2,3,4")).to eq(10)
     end
 
     it 'returns the sum of numbers separated by commas or new lines' do
-      calculator = StringCalculator.new
       expect(calculator.add("1\n2,3")).to eq(6)
     end
 
     it 'supports different delimiters' do
-      calculator = StringCalculator.new
       expect(calculator.add("//;\n1;2")).to eq(3)
       expect(calculator.add("//|\n1|2|3")).to eq(6)
     end
 
     it 'raises an exception for negative numbers' do
-      calculator = StringCalculator.new
       expect { calculator.add("1,-2,3") }.to raise_error("negatives not allowed: -2")
     end
 
     it 'shows all negative numbers in the exception message' do
-      calculator = StringCalculator.new
       expect { calculator.add("1,-2,-3") }.to raise_error("negatives not allowed: -2, -3")
     end
 
     it 'ignores numbers bigger than 1000' do
-      calculator = StringCalculator.new
       expect(calculator.add("2,1001")).to eq(2)
     end
 
     it 'supports delimiters of any length' do
-      calculator = StringCalculator.new
       expect(calculator.add("//[***]\n1***2***3")).to eq(6)
     end
 
     it 'supports multiple delimiters' do
-      calculator = StringCalculator.new
       expect(calculator.add("//[*][%]\n1*2%3")).to eq(6)
     end
 
     it 'supports multiple delimiters with length longer than one char' do
-      calculator = StringCalculator.new
       expect(calculator.add("//[**][%%]\n1**2%%3")).to eq(6)
     end
   end
