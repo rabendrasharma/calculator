94139d8e99def8215f3a09bf10702346bb95e5cc
rabendra <rabendrasharma007@gmail.com>
2024-06-04 21:12:44 +0530

    implementation for numbers greater than 1000

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -9,7 +9,7 @@ class StringCalculator
       string = parts[1]
     end
 
-    numbers = string.split(/#{delimiter}/).map(&:to_i)
+    numbers = string.split(/#{delimiter}/).map(&:to_i).reject { |n| n > 1000 }
     negatives = numbers.select { |n| n < 0 }
     raise "negatives not allowed: #{negatives.join(', ')}" unless negatives.empty?
 
