34b9c7b91503e9406a11b24c413b5e907a8a9d84
rabendra <rabendrasharma007@gmail.com>
2024-06-04 19:41:07 +0530

    Basic test cases for empty string, one number, and two numbers

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,22 @@
+# spec/string_calculator_spec.rb
+require 'rspec'
+require_relative '../lib/string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      calculator = StringCalculator.new
+      expect(calculator.add("")).to eq(0)
+    end
+
+    it 'returns the number itself for a single number' do
+      calculator = StringCalculator.new
+      expect(calculator.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of two numbers' do
+      calculator = StringCalculator.new
+      expect(calculator.add("1,2")).to eq(3)
+    end
+  end
+end
