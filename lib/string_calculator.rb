3c28c61d146f0e66d5900de9dc0c24a563d3cce1
rabendra <rabendrasharma007@gmail.com>
2024-06-04 20:20:35 +0530

    implementation to support new lines between numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,7 @@ class StringCalculator
   def add(string)
     return 0 if string.empty?
 
-    numbers = string.split(',').map(&:to_i)
+    numbers = string.split(/,|\n/).map(&:to_i)
     numbers.sum
   end
 end
